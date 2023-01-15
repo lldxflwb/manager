9052292140a1e0564d1455e136b84f6fb1da95a9
Karl.chen <484491598@qq.com>
2023-01-16 00:31:48 +0800

    调用测试方法

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-
+#include "test/NameTreeTest.h"
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    NameTreeTest name_test;
+    name_test.test();
     return 0;
 }
