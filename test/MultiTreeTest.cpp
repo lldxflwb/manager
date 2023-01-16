478e6dd9cd2daa559026c310e1e2cc14de73374f
Karl.chen <484491598@qq.com>
2023-01-16 22:03:27 +0800

    初始化测试

---
+++ test/MultiTreeTest.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 48449 on 2023/1/16.
+//
+
+#include "MultiTreeTest.h"
---
+++ test/MultiTreeTest.h
@@ -0,0 +1,14 @@
+//
+// Created by 48449 on 2023/1/16.
+//
+
+#ifndef MANAGER_MULTITREETEST_H
+#define MANAGER_MULTITREETEST_H
+
+
+class MultiTreeTest {
+
+};
+
+
+#endif //MANAGER_MULTITREETEST_H
