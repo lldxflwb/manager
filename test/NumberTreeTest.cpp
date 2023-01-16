21718ead8a0f043f525cbb8e0b6d569687a43a6a
Karl.chen <484491598@qq.com>
2023-01-16 13:56:56 +0800

    number test tree

---
+++ test/NumberTreeTest.cpp
@@ -0,0 +1,44 @@
+//
+// Created by 48449 on 2023/1/16.
+//
+
+#include "NumberTreeTest.h"
+
+void NumberTreeTest::test() {
+    insert_test();
+    query_test();
+}
+
+void NumberTreeTest::insert_test() {
+    std::cout << "开始插入，插入用户数量为：" << manager->user->size() << std::endl;
+    START_TIMER
+    for (auto & item : *((*manager).user)) {
+        tree->add_son(&item);
+    }
+    STOP_TIMER
+    std::cout << std::endl;
+}
+
+void NumberTreeTest::query_test() {
+    {
+        std::cout << "测试正向获取用户列表" <<std::endl;
+        START_TIMER
+        auto vec = tree->get_order(0);
+        for (auto & item : *vec) {
+            std::cout << *item << std::endl;
+        }
+        STOP_TIMER
+        std::cout << "测试正向获取用户列表结束"<<std::endl;
+    }
+    {
+        std::cout << "测试反向获取用户列表"<<std::endl;
+        START_TIMER
+        auto vec = tree->get_order(1);
+        for (auto & item : *vec) {
+            std::cout << (*item) << std::endl;
+        }
+        STOP_TIMER
+        std::cout << "测试反向获取用户列表结束"<<std::endl;
+    }
+}
+
---
+++ test/NumberTreeTest.h
@@ -0,0 +1,27 @@
+//
+// Created by 48449 on 2023/1/16.
+//
+
+#ifndef MANAGER_NUMBERTREETEST_H
+#define MANAGER_NUMBERTREETEST_H
+
+#include "TestAction.h"
+#include "../tree/NumberTree.h"
+
+class NumberTreeTest {
+    std::shared_ptr<Manager> manager ;
+    std::shared_ptr<NumberTree> tree ;
+
+public:
+    NumberTreeTest() {
+//        manager = TestAction::MakeManager(1000000);
+        manager = TestAction::MakeManager(100);
+        tree = std::make_shared<NumberTree>();
+    }
+    void test();
+    void insert_test();
+    void query_test();
+};
+
+
+#endif //MANAGER_NUMBERTREETEST_H
