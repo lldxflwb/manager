44318c7400df29d400afc6215fcf52a500636d3b
Karl.chen <484491598@qq.com>
2023-01-16 20:38:02 +0800

    删除节点 添加节点

--- tree/MultiNumberTree.cpp
+++ tree/MultiNumberTree.cpp
@@ -3,3 +3,120 @@
 //
 
 #include "MultiNumberTree.h"
+#include <stack>
+
+Ecode MultiNumberTree::add_son(PeopleRef people) {
+    auto q = GetIDs(*people->getId());
+    if (AddSon(people,q)){
+        return success;
+    }
+    return Ecode::insert_error;
+}
+
+Ecode MultiNumberTree::delete_son(PeopleRef people) {
+    if (people->getId() == nullptr){
+        return Ecode::no_id;
+    }
+    auto q = GetIDs(*people->getId());
+    if (this->DeleteSon(people,q)){
+        return success;
+    }
+    return delete_error;
+}
+
+std::shared_ptr<PeopleInterface> MultiNumberTree::get_user(PeopleRef people) {
+    return std::shared_ptr<PeopleInterface>();
+}
+
+std::shared_ptr<std::vector<std::shared_ptr<PeopleInterface>>> MultiNumberTree::get_peoples(PeopleRef people) {
+    return std::shared_ptr<std::vector<std::shared_ptr<PeopleInterface>>>();
+}
+
+std::shared_ptr<std::vector<std::shared_ptr<PeopleInterface>>> MultiNumberTree::get_order(int order) {
+    return std::shared_ptr<std::vector<std::shared_ptr<PeopleInterface>>>();
+}
+
+int MultiNumberTree::AddSon(PeopleRef people, std::queue<int> &q) {
+    if (q.empty() ) {
+        if ( users == nullptr ) {
+            users = std::make_shared<std::vector<PeopleRef>>();
+        }
+        // 校验是否加入，使用id
+        for(auto & item : *users ){
+            if ((*(item->getId())) == (*(people->getId()))){
+                return false;
+            }
+        }
+        users->push_back(people);
+        return true;
+    }
+    if ( son == nullptr ) {
+        son = new MultiNumberTree*[MULTI_NUMBER_TREE_MAX_NUMBER];
+        for (int i = 0; i < MULTI_NUMBER_TREE_MAX_NUMBER; ++i) {
+            son[i] = nullptr;
+        }
+    }
+    int index = q.front();
+    q.pop();
+    if ( son[index] == nullptr ) {
+        son[index] = new MultiNumberTree();
+    }
+    bool flag_result = son[index]->AddSon(people, q );
+    if ( flag_result ){
+        son_sum ++ ;
+        return true;
+    }
+    return false;
+}
+
+std::queue<int> MultiNumberTree::GetIDs(IDTYPE id) {
+    std::queue<int> q;
+    int deep = GetMaxDeep();
+    int number_deep = GetMaxDeep(id);
+    deep-=number_deep;
+    while (deep--){
+        q.push(0);
+    }
+    std::stack<int> st;
+    while(id){
+        st.push(id%MULTI_NUMBER_TREE_MAX_NUMBER);
+        id/=MULTI_NUMBER_TREE_MAX_NUMBER;
+    }
+    while(!st.empty()){
+        q.push(st.top());
+        st.pop();
+    }
+    return std::move(q);
+}
+
+int MultiNumberTree::DeleteSon(PeopleRef people, std::queue<int> &q) {
+    if ( q.empty() ) {
+        if ( users ==nullptr ) {
+            return false;
+        }
+        for( auto iter = (*users).begin(); iter != users->end() ; iter ++ ){
+            if ( (*((*iter)->getId())) == (*(people->getId())) ){
+                users->erase(iter);
+                return true;
+            }
+        }
+        return false;
+    }
+    if (son==nullptr){
+        return false;
+    }
+    if (son_sum == 0){
+        return false;
+    }
+    int index = q.front();
+    q.pop();
+    if ( son[index]== nullptr){
+        return false;
+    }
+    bool flag_result = son[index]->DeleteSon(people,q);
+    if ( flag_result ){
+        son_sum--;
+        return true;
+    }
+    return false;
+}
--- tree/MultiNumberTree.h
+++ tree/MultiNumberTree.h
@@ -6,12 +6,51 @@
 #define MANAGER_MULTINUMBERTREE_H
 #include "../modle/User.h"
 #include "../interface/MapTreeInterface.h"
-#define NUMBER_TREE_MAX_NUMBER 5l
-#define SAVE_MAX_NUMBER 1000000000l
+#define MULTI_NUMBER_TREE_MAX_NUMBER 5l
+#define MULTI_SAVE_MAX_NUMBER 1000000000l
 #include <queue>
 
-class MultiNumberTree {
+class MultiNumberTree : public MapTreeInterface {
+    MultiNumberTree ** son;
+    std::shared_ptr<std::vector<PeopleRef> > users;
+    int son_sum;
+    int AddSon(PeopleRef people, std::queue<int> &q);
+    int DeleteSon(PeopleRef people, std::queue<int> & q);
 
+    constexpr int GetMaxDeep() {
+        auto max_number = MULTI_SAVE_MAX_NUMBER;
+        int cnts = 0;
+        while(max_number){
+            max_number/=MULTI_NUMBER_TREE_MAX_NUMBER;
+            cnts++;
+        }
+        return cnts;
+    }
+    int GetMaxDeep(IDTYPE id){
+        int cnts = 0;
+        while(id){
+            id/=MULTI_NUMBER_TREE_MAX_NUMBER;
+            cnts++;
+        }
+        return cnts;
+    }
+    std::queue<int> GetIDs(IDTYPE id);
+public:
+    MultiNumberTree() {
+        son = nullptr;
+        son_sum = 0 ;
+        users = nullptr;
+    }
+
+    Ecode add_son(PeopleRef people) override; // ok
+
+    Ecode delete_son(PeopleRef people) override;
+
+    std::shared_ptr<PeopleInterface> get_user(PeopleRef people) override;
+
+    std::shared_ptr<std::vector<std::shared_ptr<PeopleInterface>>> get_peoples(PeopleRef people) override;
+
+    std::shared_ptr<std::vector<std::shared_ptr<PeopleInterface>>> get_order(int order) override;
 };
 
 
