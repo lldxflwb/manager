0897e8daaf460b4d59f0e9c70787ff10c224e175
Karl.chen <484491598@qq.com>
2023-01-16 20:05:13 +0800

    初始化多重数值字典树

---
+++ tree/MultiNumberTree.h
@@ -0,0 +1,18 @@
+//
+// Created by 48449 on 2023/1/16.
+//
+
+#ifndef MANAGER_MULTINUMBERTREE_H
+#define MANAGER_MULTINUMBERTREE_H
+#include "../modle/User.h"
+#include "../interface/MapTreeInterface.h"
+#define NUMBER_TREE_MAX_NUMBER 5l
+#define SAVE_MAX_NUMBER 1000000000l
+#include <queue>
+
+class MultiNumberTree {
+
+};
+
+
+#endif //MANAGER_MULTINUMBERTREE_H
