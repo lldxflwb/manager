e1ba51f9b638a135664943e0e462354a9bb8ad26
Karl.chen <484491598@qq.com>
2023-01-16 15:28:20 +0800

    启用测试

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 #include "test/NumberTreeTest.h"
 int main() {
     std::cout << "Hello, World!" << std::endl;
-//    NameTreeTest name_test;
-//    name_test.test();
+    NameTreeTest name_test;
+    name_test.test();
     NumberTreeTest number_test;
     number_test.test();
     return 0;
