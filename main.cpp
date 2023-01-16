9529a15a9e02640f371dbe5a1425d43d0a1eb07a
Karl.chen <484491598@qq.com>
2023-01-16 13:57:06 +0800

    number test tree

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include "test/NameTreeTest.h"
+#include "test/NumberTreeTest.h"
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    NameTreeTest name_test;
-    name_test.test();
+//    NameTreeTest name_test;
+//    name_test.test();
+    NumberTreeTest number_test;
+    number_test.test();
     return 0;
 }
